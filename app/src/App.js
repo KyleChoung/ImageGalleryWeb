48be2a382c603686717d31a67c4c18a5a6e36a18
KyleChoung <130705735+KyleChoung@users.noreply.github.com>
2023-05-09 05:14:24 +0800

    圖片瀏覽

---
+++ app/src/App.js
@@ -0,0 +1,230 @@
+import './App.css';
+import Allimages from './data';
+import React, { useState, useEffect } from 'react';
+import SortableList, { SortableKnob } from 'react-easy-sort';
+import { SortableItem } from 'react-easy-sort';
+import arrayMove from 'array-move';
+
+function App() {
+  const [tag, setTag] = useState('All');
+  const [oringinImages, setoringinImages] = useState([]);
+  const [filteredImages, setFilteredImages] = useState([]);
+  const [image, setimage] = useState('');
+  const [addimageName, setaddimageName] = useState();
+  const [addimagestag, setaddimagetag] = useState();
+
+  const handleChange = (e) => {
+    setTag(e.target.value);
+  };
+
+  const openmodal = (e) => {
+    setimage(e.target.src);
+    const infoModal = document.getElementById('infoModal');
+    infoModal.showModal();
+  };
+
+  const openmodal2 = (e) => {
+    const infoModal = document.getElementById('infoModal2');
+    infoModal.showModal();
+  };
+
+  const handleclose = (e) => {
+    const infoModal = document.getElementById('infoModal');
+    const dialogDimensions = infoModal.getBoundingClientRect();
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      infoModal.close();
+    }
+  };
+  const canceldialog = () => {
+    const infoModal2 = document.getElementById('infoModal2');
+    infoModal2.close();
+  };
+
+  const convertToBase64 = (e) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      setaddimageName(reader.result);
+    };
+  };
+
+  const submitHandler = () => {
+    const newarray = {
+      id: Math.random(),
+      imageName: addimageName,
+      tag: addimagestag,
+    };
+    const copy = [...filteredImages];
+    copy.push(newarray);
+    setFilteredImages(copy);
+    localStorage.setItem('save', JSON.stringify(copy));
+  };
+
+  const remove = () => {
+    localStorage.clear();
+    setFilteredImages([]);
+  };
+
+  useEffect(() => {
+    const saveitem = JSON.parse(localStorage.getItem('save')) || [];
+    tag === 'All'
+      ? setoringinImages(Allimages)
+      : setoringinImages(Allimages.filter((image) => image.tag === tag));
+
+    tag === 'All'
+      ? setFilteredImages(saveitem)
+      : setFilteredImages(saveitem.filter((image) => image.tag === tag));
+  }, [tag]);
+
+  useEffect(() => {
+    setFilteredImages([]);
+    const saveitem = JSON.parse(localStorage.getItem('save')) || [];
+    setoringinImages(Allimages);
+    setFilteredImages(saveitem);
+  }, []);
+
+  //Drag
+  const onSortEnd = (oldIndex, newIndex) => {
+    setoringinImages((array) => arrayMove(array, oldIndex, newIndex));
+    setFilteredImages((array) => arrayMove(array, oldIndex, newIndex));
+  };
+
+  return (
+    <div className="App">
+      <div className="tags">
+        <label style={{ color: 'white', margin: 'auto 30px' }}>
+          標籤:{' '}
+          <select onChange={handleChange} name="selected">
+            <option value="All">All</option>
+            <option value="Tree">Tree</option>
+            <option value="People">People</option>
+            <option value="Dog">Dog</option>
+          </select>
+        </label>{' '}
+        <button onClick={openmodal2}>加入圖片</button>{' '}
+        <button onClick={remove}>刪除全部加入的圖片</button>
+      </div>
+
+      <div className="container">
+        <h1>範例圖片</h1>
+        <SortableList
+          onSortEnd={onSortEnd}
+          className="list"
+          draggedItemClassName="dragged"
+        >
+          {oringinImages.map((image) => (
+            <SortableItem key={image.id} className="image-card">
+              <div className="item">
+                <SortableKnob>
+                  <div
+                    style={{
+                      position: 'absolute',
+                      textAlign: 'center',
+                      backgroundColor: 'gray',
+                      borderRadius: '10px',
+                      color: 'white',
+                    }}
+                  >
+                    Drag me
+                  </div>
+                </SortableKnob>
+                <img
+                  onClick={openmodal}
+                  className="image"
+                  src={`/images/${image.imageName}`}
+                  alt=""
+                />{' '}
+              </div>
+            </SortableItem>
+          ))}
+        </SortableList>
+        <br />
+        <h1>加入的圖片</h1>
+        <SortableList
+          onSortEnd={onSortEnd}
+          className="list"
+          draggedItemClassName="dragged"
+        >
+          {filteredImages.map((image) => (
+            <SortableItem key={image.id} className="image-card">
+              <div className="item">
+                <SortableKnob>
+                  <div
+                    style={{
+                      position: 'absolute',
+                      textAlign: 'center',
+                      backgroundColor: 'gray',
+                      borderRadius: '10px',
+                      color: 'white',
+                    }}
+                  >
+                    Drag me
+                  </div>
+                </SortableKnob>
+                <img
+                  onClick={openmodal}
+                  className="image"
+                  src={image.imageName}
+                  alt=""
+                />
+              </div>
+            </SortableItem>
+          ))}
+        </SortableList>
+      </div>
+      <dialog id="infoModal" onClick={handleclose}>
+        <img width="600" src={image} alt="" />{' '}
+      </dialog>
+
+      <dialog id="infoModal2" onClick={handleclose}>
+        <form>
+          <p style={{ position: 'flex' }}>
+            <label>
+              圖片:{' '}
+              <input
+                accept="image/*"
+                type="file"
+                onChange={convertToBase64}
+                required
+              />
+            </label>
+            <label>
+              標籤:{' '}
+              <select
+                onChange={(e) => setaddimagetag(e.target.value)}
+                name="selected"
+              >
+                <option value="select">請選擇</option>
+                <option value="Tree">Tree</option>
+                <option value="People">People</option>
+                <option value="Dog">Dog</option>
+              </select>
+            </label>{' '}
+          </p>
+          <div>
+            <button
+              id="confirmBtn"
+              type="comfirm"
+              onClick={submitHandler}
+              disabled={
+                !addimageName || !addimagestag || addimagestag === 'select'
+              }
+            >
+              確定
+            </button>
+            <button type="button" value="cancel" onClick={canceldialog}>
+              取消
+            </button>
+          </div>
+        </form>
+      </dialog>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/data.js
@@ -0,0 +1,20 @@
+const data = [
+  { id: '1', imageName: '1.jpg', tag: 'People' },
+  { id: '2', imageName: '2.jpg', tag: 'People' },
+  { id: '3', imageName: '3.jpg', tag: 'People' },
+  { id: '4', imageName: '4.jpg', tag: 'People' },
+  { id: '5', imageName: '5.jpg', tag: 'People' },
+  { id: '6', imageName: '6.jpg', tag: 'Tree' },
+  { id: '7', imageName: '7.jpg', tag: 'Tree' },
+  { id: '8', imageName: '8.jpg', tag: 'Tree' },
+  { id: '9', imageName: 'P1.jpg', tag: 'Dog' },
+  { id: '10', imageName: 'P2.jpg', tag: 'Dog' },
+  { id: '11', imageName: 'P3.jpg', tag: 'Dog' },
+  { id: '12', imageName: 'P4.jpg', tag: 'Dog' },
+  { id: '13', imageName: 'P5.jpg', tag: 'Dog' },
+  { id: '14', imageName: 'P6.jpg', tag: 'Dog' },
+  { id: '15', imageName: 'P7.jpg', tag: 'Dog' },
+  // { id: '16', imageName: 'P6.jpg', tag: 'new' },
+];
+
+export default data;
